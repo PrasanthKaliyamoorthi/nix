9e6faf64a088d51dac558b5a64a7a08a925140d3
Prasanth Kalaiyamoorthi <prasanthkaliyamoorthi2000@gmail.com>
2026-01-15 14:32:02 +0530

    test 1

--- os/conf/init.nix
+++ os/conf/init.nix
@@ -2,6 +2,6 @@
 
 {
   imports = [
-  ./sys/system.nix
+    ./sys/system.nix
   ];
-  }
+}
--- os/conf/sys/sysPkgs.nix
+++ os/conf/sys/sysPkgs.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs: [
+  environment.systemPackages = with pkgs; [
     vim
     wget
     neovim
