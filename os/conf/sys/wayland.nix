1d77b3e970a09ccf69f3fb9b1f7864343197776c
Prasanth Kalaiyamoorthi <prasanthkaliyamoorthi2000@gmail.com>
2026-01-15 18:08:28 +0530

    homw

--- os/conf/sys/wayland.nix
+++ os/conf/sys/wayland.nix
@@ -1,17 +1,13 @@
-{ pkgs, user, ... }: {
-	services.xserver = {
-		# Required by SDDM.
-		enable = true;
-		displayManager.sddm = {
-			enable = true;
-			wayland.enable = true;
-		};
-		# Enable Desktop Environment.
-		desktopManager.plasma6.enable = true;
-		# Configure keymap in X11.
-		layout = user.services.xserver.layout;
-		xkbVariant = user.services.xserver.xkbVariant;
-		# Exclude default X11 packages I don't want.
-		excludePackages = with pkgs; [ xterm ];
-	};
+{ pkgs, config, ... }: {
+  services.xserver = {
+    enable = true;
+    displayManager.sddm = {
+      enable = true;
+      wayland.enable = true;
+    };
+    desktopManager.plasma6.enable = true;
+    layout = "us";
+    xkbVariant = "";
+    excludePackages = with pkgs; [ xterm ];
+  };
 }
