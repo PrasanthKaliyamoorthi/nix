ab883546b448c148ac8bd2cdfb640091d6ee1e0d
Prasanth Kalaiyamoorthi <prasanthkaliyamoorthi2000@gmail.com>
2026-01-15 15:07:52 +0530

    test 1

--- os/conf/flake.nix
+++ os/conf/flake.nix
@@ -1,14 +1,29 @@
 {
-  description = "A very basic flake";
+  description = "My flake configuration. no idea what i'm dooing. !!!HELP";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux = {
-      default = self.packages.x86_64-linux.hello;
-      hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-    };
+  outputs = 
+    { nixpkgs, home-manager, ... }:
+    {
+      nixosConfigurations = {
+        hostname = nixpkgs.lib.nixosSystem
+        {
+            system = "x86_64-linux";
+            modules = [
+              ../configuration.nix
+              home-manager.nixosModules.home-manager
+              {
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+                home-manager.users.piero = ./home/home.nix;
+              }
+            ];
+        };
+      };
   };
 }
