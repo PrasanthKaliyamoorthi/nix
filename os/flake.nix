89ed4c3a0270f46c6d836e73ba1bd186a4528234
Prasanth Kalaiyamoorthi <prasanthkaliyamoorthi2000@gmail.com>
2026-01-15 20:02:41 +0530

    homwa

--- os/flake.nix
+++ os/flake.nix
@@ -5,6 +5,12 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    hyprland.url = "github:hyprwm/Hyprland";
+    hyprland-plugins = {
+      url = "github:hyprwm/hyprland-plugins";
+      inputs.hyprland.follows = "hyprland";
+    };
   };
 
   outputs = 
