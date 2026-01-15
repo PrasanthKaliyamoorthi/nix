89b1335322758ba886092772da2d4e3c49910672
Prasanth Kalaiyamoorthi <prasanthkaliyamoorthi2000@gmail.com>
2026-01-15 16:14:09 +0530

    homw

--- os/conf/home/home.nix
+++ os/conf/home/home.nix
@@ -131,6 +131,7 @@
     enable = true;
 
     settings = {
+      background_opacity = "0.9";
       env.TERM = "xterm-256color";
       font = {
         family = "CaskaydiaCove Nerd Font Mono";
@@ -138,8 +139,31 @@
         bold = auto;
         italic = auto;
         bold_italic = auto;
+        background_blur = 5;
+        symbol_map = let
+          mappings = [
+            "U+23FB-U+23FE"
+            "U+2B58"
+            "U+E200-U+E2A9"
+            "U+E0A0-U+E0A3"
+            "U+E0B0-U+E0BF"
+            "U+E0C0-U+E0C8"
+            "U+E0CC-U+E0CF"
+            "U+E0D0-U+E0D2"
+            "U+E0D4"
+            "U+E700-U+E7C5"
+            "U+F000-U+F2E0"
+            "U+2665"
+            "U+26A1"
+            "U+F400-U+F4A8"
+            "U+F67C"
+            "U+E000-U+E00A"
+            "U+F300-U+F313"
+            "U+E5FA-U+E62B"
+          ];
+        in
+          (builtins.concatStringsSep "," mappings) + " Symbols Nerd Font";
       }
-      background.opacity = 9.0;
     }
   }
 
