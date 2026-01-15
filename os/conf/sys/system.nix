4f0cc697a482a0d893d8eca751fe4d007fb44062
Prasanth Kalaiyamoorthi <prasanthkaliyamoorthi2000@gmail.com>
2026-01-15 14:38:16 +0530

    test 1

--- os/conf/sys/system.nix
+++ os/conf/sys/system.nix
@@ -17,12 +17,13 @@
   time.timeZone = "Asia/Kolkata";
   
   # user account.
-  users.users.nix = {
+  users.users.piero = {
     isNormalUser = true;
     extraGroups = [ "wheel" ]; # enable sudo
     packages = with pkgs; [
       tree
     ];
+    hashedPassword = "$6$hWrbtUY8FdNmVQAk$apvP7wY95ciYCWxeY4ptmQntRK31Uh8/3QLEXPUKxJY8njcnH8agSf8QoIF3aCAz5njunI0g2SSU87QZ9MnF21";
   };
 
   programs.mtr.enable = true;
